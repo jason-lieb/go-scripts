a43f1e9651001bb92923db309ebe8b815f795e06
Jason Lieb <Jason.lieb@outlook.com>
2024-09-14 00:52:45 -0400

    uncomment make-imports-absolute and get to compile

--- scripts/make-imports-absolute/make-imports-absolute/main.go
+++ scripts/make-imports-absolute/make-imports-absolute/main.go
@@ -2,67 +2,69 @@ package main
 
 import (
 	"fmt"
-	// "os"
-	// "path/filepath"
-	// "regexp"
-	// "strings"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 func main() {
-    fmt.Println("World flake")
-	// 	if len(os.Args) != 1 {
-	// 		fmt.Println("Please provide absolute import path")
-	// 		os.Exit(1)
-	// 	}
+	if len(os.Args) != 2 {
+		fmt.Println("Please provide absolute import path")
+		os.Exit(1)
+	}
 
-    // if err := filepath.Walk("./", visit); err != nil {
-    //     panic(err)
-    // }
+	absolutePath := os.Args[1]
+
+	if err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
+		return visit(absolutePath, path, info, err)
+	}); err != nil {
+		panic(err)
+	}
 }
 
-// func visit(path string, info os.FileInfo, err error) error {
-//     if err != nil {
-//         return err
-//     }
+func visit(absolutePath string, path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
 
-//     if info.IsDir() || !(strings.HasSuffix(path, ".ts") || strings.HasSuffix(path, ".tsx")) {
-//         return nil
-//     }
+	if info.IsDir() || !(strings.HasSuffix(path, ".ts") || strings.HasSuffix(path, ".tsx")) {
+		return nil
+	}
 
-//     if err := changeRelativeImportsToAbsolute(path); err != nil {
-//         return err
-//     }
+	if err := changeRelativeImportsToAbsolute(path, absolutePath); err != nil {
+		return err
+	}
 
-//     return nil
-// }
+	return nil
+}
 
+func changeRelativeImportsToAbsolute(filePath string, absolutePath string) error {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
 
-// func changeRelativeImportsToAbsolute(filePath string) error {
-//     content, err := os.ReadFile(filePath)
-//     if err != nil {
-//         return err
-//     }
+	modifiedContent := modifyImports(filePath, string(content))
 
-// 		modifiedContent := modifyImports(filePath, string(content))
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-//     file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC, 0666)
-//     if err != nil {
-//         return err
-//     }
-//     defer file.Close()
+	if _, err := file.WriteString(modifiedContent); err != nil {
+		return err
+	}
 
-// 		if _, err := file.WriteString(modifiedContent); err != nil {
-//         return err
-//     }
+	return nil
+}
 
-//     return nil
-// }
-
-// func modifyImports(filePath string, content string) string {
-// 		// sample filePaths
-// 		// file1.ts
-// 		// subdir/file2.ts
-// 		absoluteImportPath := os.Args[1]
-//     regex := regexp.MustCompile(`from '(\..*?)'`)
-//     return regex.ReplaceAllString(content, `from "`+absoluteImportPath+`$1";`)
-// }
+func modifyImports(filePath string, content string) string {
+	// sample filePaths
+	// file1.ts
+	// subdir/file2.ts
+	absoluteImportPath := os.Args[1]
+	regex := regexp.MustCompile(`from '(\..*?)'`)
+	return regex.ReplaceAllString(content, `from "`+absoluteImportPath+`$1";`)
+}
